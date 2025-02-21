19155268925cd689421ff3b6169a710603a24238
Lindsay Warrenburg <lindsay.a.warrenburg@gmail.com>
2025-02-21 14:33:45 -0500

    eligibility modified to our cohort and final models working

--- models/final/medical_claim.sql
+++ models/final/medical_claim.sql
@@ -1,4 +1,11 @@
-with unioned as (
+-- this is modified from the tuva one
+
+with eligibility as (
+    select patient_id
+    from {{ ref('eligibility') }}
+)
+
+, unioned as (
 
     {{ dbt_utils.union_relations(relations=[
           ref('carrier_claim')
@@ -11,187 +18,153 @@ with unioned as (
     ],
     exclude=["_DBT_SOURCE_RELATION"]
 ) }}
-
 )
 
-, month_start_and_end_dates as (
-
-    select distinct
-          year as year_nbr
-        , month as month_nbr
-        , first_day_of_month
-        , last_day_of_month
-    from {{ ref('reference_data__calendar') }}
-
-)
-
-, member_months as (
-
-    select distinct
-          a.person_id
-        , year_nbr
-        , month_nbr
-        , a.payer
-        , a.plan
-        , data_source
-    from {{ ref('eligibility') }} as a
-        inner join month_start_and_end_dates as b
-            on a.enrollment_start_date <= b.last_day_of_month
-            and a.enrollment_end_date >= b.first_day_of_month
-
-)
-
--- limit claims to only enrolled members, removing claims for members that we only have partial claims data for.
 select
-      c.claim_id
-    , c.claim_line_number
-    , c.claim_type
-    , c.person_id
-    , c.member_id
-    , c.payer
-    , c.plan
-    , c.claim_start_date
-    , c.claim_end_date
-    , c.claim_line_start_date
-    , c.claim_line_end_date
-    , c.admission_date
-    , c.discharge_date
-    , c.admit_source_code
-    , c.admit_type_code
-    , c.discharge_disposition_code
-    , c.place_of_service_code
-    , c.bill_type_code
-    , c.ms_drg_code
-    , c.apr_drg_code
-    , c.revenue_center_code
-    , c.service_unit_quantity
-    , c.hcpcs_code
-    , c.hcpcs_modifier_1
-    , c.hcpcs_modifier_2
-    , c.hcpcs_modifier_3
-    , c.hcpcs_modifier_4
-    , c.hcpcs_modifier_5
-    , c.rendering_npi
-    , c.rendering_tin
-    , c.billing_npi
-    , c.billing_tin
-    , c.facility_npi
-    , c.paid_date
-    , c.paid_amount
-    , c.allowed_amount
-    , c.charge_amount
-    , c.coinsurance_amount
-    , c.copayment_amount
-    , c.deductible_amount
-    , c.total_cost_amount
-    , c.diagnosis_code_type
-    , c.diagnosis_code_1
-    , c.diagnosis_code_2
-    , c.diagnosis_code_3
-    , c.diagnosis_code_4
-    , c.diagnosis_code_5
-    , c.diagnosis_code_6
-    , c.diagnosis_code_7
-    , c.diagnosis_code_8
-    , c.diagnosis_code_9
-    , c.diagnosis_code_10
-    , c.diagnosis_code_11
-    , c.diagnosis_code_12
-    , c.diagnosis_code_13
-    , c.diagnosis_code_14
-    , c.diagnosis_code_15
-    , c.diagnosis_code_16
-    , c.diagnosis_code_17
-    , c.diagnosis_code_18
-    , c.diagnosis_code_19
-    , c.diagnosis_code_20
-    , c.diagnosis_code_21
-    , c.diagnosis_code_22
-    , c.diagnosis_code_23
-    , c.diagnosis_code_24
-    , c.diagnosis_code_25
-    , c.diagnosis_poa_1
-    , c.diagnosis_poa_2
-    , c.diagnosis_poa_3
-    , c.diagnosis_poa_4
-    , c.diagnosis_poa_5
-    , c.diagnosis_poa_6
-    , c.diagnosis_poa_7
-    , c.diagnosis_poa_8
-    , c.diagnosis_poa_9
-    , c.diagnosis_poa_10
-    , c.diagnosis_poa_11
-    , c.diagnosis_poa_12
-    , c.diagnosis_poa_13
-    , c.diagnosis_poa_14
-    , c.diagnosis_poa_15
-    , c.diagnosis_poa_16
-    , c.diagnosis_poa_17
-    , c.diagnosis_poa_18
-    , c.diagnosis_poa_19
-    , c.diagnosis_poa_20
-    , c.diagnosis_poa_21
-    , c.diagnosis_poa_22
-    , c.diagnosis_poa_23
-    , c.diagnosis_poa_24
-    , c.diagnosis_poa_25
-    , c.procedure_code_type
-    , c.procedure_code_1
-    , c.procedure_code_2
-    , c.procedure_code_3
-    , c.procedure_code_4
-    , c.procedure_code_5
-    , c.procedure_code_6
-    , c.procedure_code_7
-    , c.procedure_code_8
-    , c.procedure_code_9
-    , c.procedure_code_10
-    , c.procedure_code_11
-    , c.procedure_code_12
-    , c.procedure_code_13
-    , c.procedure_code_14
-    , c.procedure_code_15
-    , c.procedure_code_16
-    , c.procedure_code_17
-    , c.procedure_code_18
-    , c.procedure_code_19
-    , c.procedure_code_20
-    , c.procedure_code_21
-    , c.procedure_code_22
-    , c.procedure_code_23
-    , c.procedure_code_24
-    , c.procedure_code_25
-    , c.procedure_date_1
-    , c.procedure_date_2
-    , c.procedure_date_3
-    , c.procedure_date_4
-    , c.procedure_date_5
-    , c.procedure_date_6
-    , c.procedure_date_7
-    , c.procedure_date_8
-    , c.procedure_date_9
-    , c.procedure_date_10
-    , c.procedure_date_11
-    , c.procedure_date_12
-    , c.procedure_date_13
-    , c.procedure_date_14
-    , c.procedure_date_15
-    , c.procedure_date_16
-    , c.procedure_date_17
-    , c.procedure_date_18
-    , c.procedure_date_19
-    , c.procedure_date_20
-    , c.procedure_date_21
-    , c.procedure_date_22
-    , c.procedure_date_23
-    , c.procedure_date_24
-    , c.procedure_date_25
-    , c.in_network_flag
-    , c.data_source
-    , c.file_name
-    , c.ingest_datetime
-from unioned as c
-    inner join member_months as mm
-        on c.person_id = mm.person_id
-        and extract(year from c.claim_start_date) = mm.year_nbr
-        and extract(month from c.claim_start_date) = mm.month_nbr
+      u.claim_id
+    , u.claim_line_number
+    , u.claim_type
+    , u.person_id
+    , u.member_id
+    , u.payer
+    , u.plan
+    , u.claim_start_date
+    , u.claim_end_date
+    , u.claim_line_start_date
+    , u.claim_line_end_date
+    , u.admission_date
+    , u.discharge_date
+    , u.admit_source_code
+    , u.admit_type_code
+    , u.discharge_disposition_code
+    , u.place_of_service_code
+    , u.bill_type_code
+    , u.ms_drg_code
+    , u.apr_drg_code
+    , u.revenue_center_code
+    , u.service_unit_quantity
+    , u.hcpcs_code
+    , u.hcpcs_modifier_1
+    , u.hcpcs_modifier_2
+    , u.hcpcs_modifier_3
+    , u.hcpcs_modifier_4
+    , u.hcpcs_modifier_5
+    , u.rendering_npi
+    , u.rendering_tin
+    , u.billing_npi
+    , u.billing_tin
+    , u.facility_npi
+    , u.paid_date
+    , u.paid_amount
+    , u.allowed_amount
+    , u.charge_amount
+    , u.coinsurance_amount
+    , u.copayment_amount
+    , u.deductible_amount
+    , u.total_cost_amount
+    , u.diagnosis_code_type
+    , u.diagnosis_code_1
+    , u.diagnosis_code_2
+    , u.diagnosis_code_3
+    , u.diagnosis_code_4
+    , u.diagnosis_code_5
+    , u.diagnosis_code_6
+    , u.diagnosis_code_7
+    , u.diagnosis_code_8
+    , u.diagnosis_code_9
+    , u.diagnosis_code_10
+    , u.diagnosis_code_11
+    , u.diagnosis_code_12
+    , u.diagnosis_code_13
+    , u.diagnosis_code_14
+    , u.diagnosis_code_15
+    , u.diagnosis_code_16
+    , u.diagnosis_code_17
+    , u.diagnosis_code_18
+    , u.diagnosis_code_19
+    , u.diagnosis_code_20
+    , u.diagnosis_code_21
+    , u.diagnosis_code_22
+    , u.diagnosis_code_23
+    , u.diagnosis_code_24
+    , u.diagnosis_code_25
+    , u.diagnosis_poa_1
+    , u.diagnosis_poa_2
+    , u.diagnosis_poa_3
+    , u.diagnosis_poa_4
+    , u.diagnosis_poa_5
+    , u.diagnosis_poa_6
+    , u.diagnosis_poa_7
+    , u.diagnosis_poa_8
+    , u.diagnosis_poa_9
+    , u.diagnosis_poa_10
+    , u.diagnosis_poa_11
+    , u.diagnosis_poa_12
+    , u.diagnosis_poa_13
+    , u.diagnosis_poa_14
+    , u.diagnosis_poa_15
+    , u.diagnosis_poa_16
+    , u.diagnosis_poa_17
+    , u.diagnosis_poa_18
+    , u.diagnosis_poa_19
+    , u.diagnosis_poa_20
+    , u.diagnosis_poa_21
+    , u.diagnosis_poa_22
+    , u.diagnosis_poa_23
+    , u.diagnosis_poa_24
+    , u.diagnosis_poa_25
+    , u.procedure_code_type
+    , u.procedure_code_1
+    , u.procedure_code_2
+    , u.procedure_code_3
+    , u.procedure_code_4
+    , u.procedure_code_5
+    , u.procedure_code_6
+    , u.procedure_code_7
+    , u.procedure_code_8
+    , u.procedure_code_9
+    , u.procedure_code_10
+    , u.procedure_code_11
+    , u.procedure_code_12
+    , u.procedure_code_13
+    , u.procedure_code_14
+    , u.procedure_code_15
+    , u.procedure_code_16
+    , u.procedure_code_17
+    , u.procedure_code_18
+    , u.procedure_code_19
+    , u.procedure_code_20
+    , u.procedure_code_21
+    , u.procedure_code_22
+    , u.procedure_code_23
+    , u.procedure_code_24
+    , u.procedure_code_25
+    , u.procedure_date_1
+    , u.procedure_date_2
+    , u.procedure_date_3
+    , u.procedure_date_4
+    , u.procedure_date_5
+    , u.procedure_date_6
+    , u.procedure_date_7
+    , u.procedure_date_8
+    , u.procedure_date_9
+    , u.procedure_date_10
+    , u.procedure_date_11
+    , u.procedure_date_12
+    , u.procedure_date_13
+    , u.procedure_date_14
+    , u.procedure_date_15
+    , u.procedure_date_16
+    , u.procedure_date_17
+    , u.procedure_date_18
+    , u.procedure_date_19
+    , u.procedure_date_20
+    , u.procedure_date_21
+    , u.procedure_date_22
+    , u.procedure_date_23
+    , u.procedure_date_24
+    , u.procedure_date_25
+    , u.in_network_flag
+from unioned as u
+    inner join eligibility as e
+        on u.person_id = e.patient_id
---
+++ src/cohort.py
@@ -0,0 +1,64 @@
+####################################################
+# if we modify the eligibility / cohort criteria,
+# re-run this py script and then re-seed with
+# dbt seed -s cohort.csv
+####################################################
+
+import polars as pl
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# cohort, but including decedents
+denom18_path = "/nfs/turbo/ihpi-cms/Wiens_ML/parquet_data/den18p20.parquet/*.parquet"
+denom19_path = "/nfs/turbo/ihpi-cms/Wiens_ML/parquet_data/den19p20.parquet/*.parquet"
+
+# relevant columns
+cols = [
+    "BENE_ID",
+    "AGE_AT_END_REF_YR", "BENE_DEATH_DT",
+    "ENTLMT_RSN_ORIG", "ENTLMT_RSN_CURR",
+    "BENE_HI_CVRAGE_TOT_MONS", "BENE_SMI_CVRAGE_TOT_MONS",
+    "BENE_HMO_CVRAGE_TOT_MONS",
+    "DUAL_ELGBL_MONS"
+]
+
+def filter_2018(df):
+    return (
+        df.filter(
+            (pl.col("BENE_HMO_CVRAGE_TOT_MONS") == 0.0) &
+            (pl.col("DUAL_ELGBL_MONS") == 0.0) &
+            (pl.col("BENE_SMI_CVRAGE_TOT_MONS") == 12.0) &
+            (pl.col("BENE_HI_CVRAGE_TOT_MONS") == 12.0) &
+            (pl.col("ENTLMT_RSN_ORIG").is_in(["0", "1"])) &
+            (pl.col("ENTLMT_RSN_CURR").is_in(["0", "1"])) & 
+            (pl.col("AGE_AT_END_REF_YR") >= 65) &
+            (pl.col("BENE_DEATH_DT").is_null())
+        )
+    )
+
+def filter_2019(df):
+    return (
+        df.filter(
+            (pl.col("BENE_HMO_CVRAGE_TOT_MONS") == 0.0) &
+            (pl.col("DUAL_ELGBL_MONS") == 0.0) &
+            (pl.col("ENTLMT_RSN_ORIG").is_in(["0", "1"])) &
+            (pl.col("ENTLMT_RSN_CURR").is_in(["0", "1"]))
+        )
+    )
+
+denom18 = pl.scan_parquet(denom18_path).rename({"bene_id": "BENE_ID"}).select(cols)
+denom19 = pl.scan_parquet(denom19_path).select(cols)
+
+benes18 = filter_2018(denom18)
+benes19 = filter_2019(denom19)
+
+benes18 = benes18.select(["BENE_ID"]).collect()
+benes19 = benes19.select(["BENE_ID"]).collect()
+
+original_cohort = list(
+    set(benes18["BENE_ID"].to_list()) & set(benes19["BENE_ID"].to_list())
+)
+
+df = pl.DataFrame({"patient_id": original_cohort})
+df.write_csv('/nfs/turbo/ihpi-cms/Wiens_ML/users/lindsay/tuva-preprocessing/seeds/cohort.csv')
